672b6ec78ba9478dbe66da6654945046201c783f
jeffersonrolino <jefferson.rolino@gmail.com>
2023-10-19 21:28:08 -0300

    Listando os produtos

--- produto/views.py
+++ produto/views.py
@@ -3,10 +3,14 @@ from django.shortcuts import render
 from django.views import View
 from django.views.generic.list import ListView
 
+from . import models
 
-class ListaProdutos(View):
-    def get(self, *args, **kwargs):
-        return HttpResponse('Listar Produtos')
+
+class ListaProdutos(ListView):
+    model = models.Produto
+    template_name = 'produto/lista.html'
+    context_object_name = 'produtos'
+    paginate_by = 9
 
 
 class DetalheProduto(View):
